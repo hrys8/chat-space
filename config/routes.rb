8593fc94631675c4f02a1c7f5b98b426091be422
Yoshioka Hiroyasu <yoshiokahiroyasu@MacBook-Pro-hrys8x.local>
2018-02-18 16:49:36 +0900

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resource :user, only: [:edit, :update]
+  resources :user, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
 end
