b3fb58edfb2387a6350e09633480229d4695adbd
Yoshioka Hiroyasu <yoshiokahiroyasu@MacBook-Pro-hrys8x.local>
2018-02-19 19:02:31 +0900

    Create group function

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
 has_many :messages
-has_many :members
 has_many :groups, through: :members
+has_many :members
 end
