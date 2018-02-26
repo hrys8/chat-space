e6e3c0dc8b9bdad6e24189c17de9a3ce851f03a4
Yoshioka Hiroyasu <yoshiokahiroyasu@MacBook-Pro-hrys8x.local>
2018-02-26 14:26:12 +0900

    Edit js action after message submit
    JS action caused by message form submit button

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,26 @@
 $(function(){
-
+  function buildHTML(message){
+    var image = (message.image_url == null) ? "" : `<img src="${message.image_url}">`
+    var html = `<div class='message'>
+                  <div class='message__upper'>
+                    <div class='message__upper_user-name'>
+                      <p>${message.user}</p>
+                    </div>
+                    <div class='message__upper_time-at'>
+                      <p>${message.created_at}</p>
+                    </div>
+                  </div>
+                  <div class='message__lower'>
+                    <p class='message__lower_text'>
+                      ${message.content}
+                    </p>
+                    <div class='lower-message__image'>
+                      ${image}
+                    </div>
+                  </div>
+                </div>`
+    return html;
+  }
   $('#form_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,6 +32,13 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__message').val('');
+      $('.hidden').val('');
+      $('.form__submit').prop('disabled', false);
     })
   });
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,4 @@
 json.user @message.user.name
-json.created_at @message.created_at
+json.created_at @message.created_at.strftime('%Y/%m/%d %T')
 json.content @message.content
 json.image_url @message.image.url
