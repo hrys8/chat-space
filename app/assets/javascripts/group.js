8aa8036d8406958cbb41c70f60caf88bc8e588da
Your Name <you@example.com>
2018-03-02 21:52:46 +0900

    Add "chat member add / remove function"

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,5 +1,14 @@
 $(function(){
 
+  function addUser(id, name){
+    var html = `<div class='chat-group-user clearfix js-chat-member' data-user-id="${id}" data-user-name="${name}">
+                  <input name="" type="hidden" value="${id}">
+                  <p class="chat-group-user__name">${name}</p>
+                  <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</a>
+                </div>`
+    $('#chat-group-users.clearfix').append(html);
+  }
+
   function appendUser(user){
     var html =  `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
@@ -12,6 +21,15 @@ $(function(){
     var html = `<div class="chat-group-user clearfix">${user}</div>`
     $("#user-search-result").append(html);
   }
+  $(document).on("click", ".chat-group-user__btn--add", function(){
+    var id = $(this).attr('data-user-id');
+    var name = $(this).attr('data-user-name');
+    addUser(id, name);
+    $(this).parent().remove();
+  });
+  $(document).on("click", ".js-remove-btn", function(){
+    $(this).parent().remove();
+  });
 
   $("#user-search-field").on("keyup", function(){
     var input = $("#user-search-field").val();
